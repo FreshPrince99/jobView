267e544aa68e4adda9c0725723d5678175f2ff7d
Aaditya Nair <aaditya.nair2412@gmail.com>
2025-05-04 16:42:58 +0100

    Added emoji functionalities

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,8 +3,12 @@ import { StyleSheet, View } from "react-native";
 
 import Button from "@/components/Button";
 import CircleButton from '@/components/CircleButton';
+import EmojiList from '@/components/EmojiList';
+import EmojiPicker from '@/components/EmojiPicker';
+import EmojiSticker from '@/components/EmojiSticker';
 import IconButton from '@/components/IconButton';
 import ImageViewer from "@/components/ImageViewer";
+import { type ImageSource } from 'expo-image';
 import { useState } from 'react';
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
@@ -12,6 +16,8 @@ const PlaceholderImage = require('@/assets/images/background-image.png');
 export default function Index() {
   const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined); // Over here the ones in the <> specify what the state variable can be and () refers to the initial value
   const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+  const [pickedEmoji, setPickedEmoji] = useState<ImageSource | undefined>(undefined);
 
   const pickImageAsync = async() => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -33,8 +39,12 @@ export default function Index() {
   };
 
   const onAddSticker = () => {
-
+    setIsModalVisible(true);
   };
+
+  const onModalClose = () => {
+    setIsModalVisible(false);
+  }
 
   const onSaveImageAsync = () => {
 
@@ -44,6 +54,7 @@ export default function Index() {
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
+        {pickedEmoji && <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />}
       </View>
       {showAppOptions ? (
         <View style={styles.optionsContainer}>
@@ -59,6 +70,9 @@ export default function Index() {
           <Button label="Use this photo" onPress={() => setShowAppOptions(true)}/>
         </View>
       )}
+      <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+        <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
+      </EmojiPicker>
     </View>
   )
 }
